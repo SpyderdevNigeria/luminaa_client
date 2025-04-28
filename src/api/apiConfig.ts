5b870aab35ff643c676707681c37be1289ed84bb
Samuel Jarvis <samueladeyemi244@gmail.com>
2025-04-28 16:12:50 +0100

    add request interceptor to include authorization token in headers

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -10,4 +10,16 @@ const api = axios.create({
 
 api.defaults.withCredentials = true;
 
+// add token to request header
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
