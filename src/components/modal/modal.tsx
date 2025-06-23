c393f2970f711c0a831492fa043818587399cdfb
emekaenyinnia <emekaenyinnia123@gmail.com>
2025-06-23 09:03:11 +0100

    add scrollbar to modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -35,7 +35,7 @@ const Modal = ({
               </AlertDialog.Title>
             </div>
           )}
-          <div className="w-full  max-h-[400px] 2xl:max-h-[500px] overflow-y-scroll">{children}</div>
+          <div className="w-full  max-h-[400px] 2xl:max-h-[500px] overflow-y-scroll scrollbar-visible ">{children}</div>
 
 
           {!hideCancel && (
